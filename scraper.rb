7fd54474c0316e04b99a499d8bb0eca32f94b604
Clement Perier Muzet <barceloneta08003@gmail.com>
2021-04-20 18:55:19 +0200

    .strip added

--- scraper.rb
+++ scraper.rb
@@ -7,13 +7,13 @@ class Scraper
   parsed_page = Nokogiri::HTML(doc)
 
   ipad_card = parsed_page.search("._31NccdTr._3CzsEFc12EYds_T_qwMJva").each do |element|
-    names = element.search("._2RGsPtNo._3lpH2rfnyK5lbG1nNzuHdX").text
+    names = element.search("._2RGsPtNo._3lpH2rfnyK5lbG1nNzuHdX").text.strip
     prices = element.search("._3OcKBk8D._3MDjp2ItWod8Ii3KjoFBIJ").text.to_i
     # years = element.search("._3OcKBk8D.y7fDboA8_-UMGGxPRpiU_").text.to_i
 
   # ipad_card = [names, prices, years]
   ipads << ipad_card
 
-    p "#{names} #{prices}€"
+    puts "#{names} > #{prices}€"
   end
 end
